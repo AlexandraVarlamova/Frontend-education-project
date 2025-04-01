c714c67c897403a7effa1e1929f46b9519ede251
Варламов Максим Федорович <m.kleschev@platformaofd.ru>
2025-04-01 09:48:41 +0300

    Свайпер

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ let brandGallery = document.querySelector ('.brand-gallery');
 button.addEventListener('click',  function onButtonClick() {
     
     brandGallery.classList.toggle('expand');
+    button.classList.toggle('expand');
 
-    if (brandGallery.classList.contains("expand")) {
+    if (brandGallery.classList.contains('expand')) {
       button.textContent = "Скрыть";
     } else {
       button.textContent = "Показать все";
     }
   });
-
   
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 0, // Устанавливаем 0, так как используем margin-right в слайдах
+    freeMode: true,
+    resistanceRatio: 0, // Для более плавной прокрутки
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            enabled: false,
+            pagination: false
+        }
+    }
+});
